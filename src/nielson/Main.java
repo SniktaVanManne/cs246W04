c7452acfe2e982b2f33cbb1f967726bcc6a56b9e
Ryan Nielson <nie04011@byui.edu>
2019-05-16 12:29:03 -0600

    Updated Syntax

--- src/nielson/Main.java
+++ src/nielson/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Ryan");
-	System.out.println("Hey guys, don't forget to pull! :)); 
+	System.out.println("Hey guys, don't forget to pull! :)" );
     }
 }
