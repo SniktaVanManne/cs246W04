5216b6ca2d09674b62484e19d13424c6a1f3c0ec
margies <ser17004@byui.edu>
2019-05-17 08:30:36 +0800

    commit Margareth's changes

--- src/nielson/Main.java
+++ src/nielson/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Ryan");
 	System.out.println("Hey guys, don't forget to pull! :)" );
+
+      System.out.println("Hello, Ryan, my name is Margareth");
     }
 }
